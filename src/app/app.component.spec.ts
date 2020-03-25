c2e6183e9e99f48114d0af8475d20aeab21b2f64
Mike Zanchettin <mike.zanchettin@gmail.com>
2020-03-24 20:59:34 -0500

    Add travis setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngfront app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('NGFRONT');
   });
 });
